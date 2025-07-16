baaee36676d18d93b1bbe6fa33dcd3808ef12a09
Miguel <miguelpereztrujillo99@gmail.com>
2025-07-16 14:55:04 +0200

    Extrar generate token logic to JwtTokenProvider, to separarte responsabilities.

--- src/main/kotlin/com/example/domain/usecases/LoginPatientUseCase.kt
+++ src/main/kotlin/com/example/domain/usecases/LoginPatientUseCase.kt
@@ -1,32 +1,17 @@
 package com.example.domain.usecases
 
 import com.example.domain.repositories.IUserRepository
-import io.jsonwebtoken.Jwts
-import io.jsonwebtoken.SignatureAlgorithm
-import io.jsonwebtoken.security.Keys
+import com.example.domain.services.JwtTokenProvider
 import org.mindrot.jbcrypt.BCrypt
-import java.util.Base64
-import java.util.Date
-import javax.crypto.SecretKey
 
 class LoginPatientUseCase(
-    private val userRepository: IUserRepository
+    private val userRepository: IUserRepository,
+    private val jwtTokenProvider: JwtTokenProvider = JwtTokenProvider()
 ) : ILoginPatientUseCase {
     override suspend fun login(email: String, password: String): String? {
         val user = userRepository.findByEmail(email)
         return if (user != null && BCrypt.checkpw(password, user.hashPassword)) {
-            val now = System.currentTimeMillis()
-            val exp = now + 3600000 // 1 hora
-            val secret = System.getenv("JWT_SECRET") ?: "V4X9N1GpQzFvZDhKRWpTeUJQYnVmVlpvTmRxSmw3dUtZbTRKcEZLeHdLMHRaM0Nx"
-            val decodedSecret = Base64.getDecoder().decode(secret)
-            require(decodedSecret.size >= 32) { "La clave secreta debe tener al menos 256 bits (32 bytes)." }
-            val secretKey: SecretKey = Keys.hmacShaKeyFor(decodedSecret)
-            Jwts.builder()
-                .setSubject(email)
-                .setExpiration(Date(exp))
-                .claim("role", "web_anon")
-                .signWith(SignatureAlgorithm.HS256, secretKey)
-                .compact()
+            jwtTokenProvider.generateToken(email, "web_anon", 3600000)
         } else {
             null
         }
